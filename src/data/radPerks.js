fe8ad77c56d4eacbb042d89f267d695633ae5fa9
MrEnderml <mr.laptev.goga@gmail.com>
2025-04-22 00:08:57 +0300

    Add Radiation.vue

---
+++ src/data/radPerks.js
@@ -0,0 +1,111 @@
+export const perks = [
+  {
+    id: 1,
+    name: 'Mutagen [T1]',
+    description: '+2% to Mutagen [T1]',
+    level: 0,
+    max: 30,
+    baseCost: 2,
+    costPerLevel: 2
+  },
+  {
+    id: 2,
+    name: 'Mutagen [T2]',
+    description: '+1.5% to Mutagen [T2]',
+    level: 0,
+    max: 30,
+    baseCost: 4,
+    costPerLevel: 4
+  },
+  {
+    id: 3,
+    name: 'Mutagen [T3]',
+    description: '+1% to Mutagen [T3]',
+    level: 0,
+    max: 30,
+    baseCost: 8,
+    costPerLevel: 8
+  },
+  {
+    id: 4,
+    name: 'Mutagen [T4]',
+    description: '+0.5% to Mutagen [T4]',
+    level: 0,
+    max: 30,
+    baseCost: 16,
+    costPerLevel: 16
+  },
+  {
+    id: 5,
+    name: 'Mutation Vortex',
+    description: 'Increase mutagen gaining [1]',
+    level: 0,
+    max: 25,
+    baseCost: 8,
+    costPerLevel: 12
+  },
+  {
+    id: 6,
+    name: 'Nuclear Resilience',
+    description: '-1 STAGE',
+    level: 0,
+    max: 5,
+    baseCost: 40,
+    costPerLevel: 40
+  },
+  {
+    id: 7,
+    name: 'Ionizer',
+    description: 'REBUILD REBIRTH SYSTEM THAT ALLOWS YOU TO SPEND MUTAGEN TO UP YOUR POTENTIAL',
+    level: 0,
+    max: 1,
+    baseCost: 250,
+    costPerLevel: 0
+  },
+  {
+    id: 8,
+    name: 'Gamma Breakthrough',
+    description: 'REBUILD ONE OF THE PERK IN TREE [T1]',
+    level: 0,
+    max: 1,
+    baseCost: 500,
+    costPerLevel: 0
+  },
+  {
+    id: 9,
+    name: 'Alpha Burst',
+    description: 'UNLOCK 4 SPECIAL PERKS IN ASCENSION',
+    level: 0,
+    max: 1,
+    baseCost: 150,
+    costPerLevel: 0
+  },
+  {
+    id: 10,
+    name: 'Radionite',
+    description: 'SOULS DONT DISAPPEAR WHEN YOU LOSE THE FIGHT BUT YOU CAN LEAVE IT',
+    level: 0,
+    max: 1,
+    baseCost: 100,
+    costPerLevel: 0
+  },
+  {
+    id: 11,
+    name: 'Irradiated Presence',
+    description: 'UNLOCK DANGER SYSTEM',
+    descriptionNext: '+1 DANGER per Level',
+    level: 0,
+    max: 100,
+    baseCost: 10,
+    costPerLevel: 5
+  },
+  {
+    id: 12,
+    name: 'Crystallized Genome',
+    description: 'CORRUPTION IS WEAKER PER EACH KILLED BOSS',
+    level: 0,
+    max: 1,
+    baseCost: 1000,
+    costPerLevel: 0
+  },
+]
